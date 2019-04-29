551ad67c71475f910a672010f60441705980cea7
CarinaEtec <miguel.sgarbozza@hotmail.com>
2019-04-28 21:59:25 -0300

    Até cadastrar autor funcionando

---
+++ ExemploMVC/BO/AutorBO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// using p/ BO pois ele acessa os 2 níveis, Model e DAO
+using ExemploMVC.DAO;
+using ExemploMVC.Model;
+
+namespace ExemploMVC.BO
+{
+    public class AutorBO
+    {
+        public void Gravar(Autor autor)
+        {
+            AutorDAO autorDao = new AutorDAO();
+            if ((autor.Nome != "") && (autor.Nacionalidade != ""))
+            {
+                autorDao.Insert(autor);
+            }
+        }
+    }
+}
---
+++ ExemploMVC/Model/Livro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploMVC.Model
+{
+    public class Livro //add public para q as outras classes possam enxergar
+    {
+        private int livroId;
+        private string titulo;
+        private DateTime datapublicacao;
+        private Autor autor; // foi criado autor do tipo Autor
+
+        public Livro()
+        {
+            Autor = new Autor(); //instanciando o autor dentro do construtor
+            // no momento em que o Livro for instanciado o autor também será instanciado
+        }
+
+        public int LivroId { get => livroId; set => livroId = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public DateTime Datapublicacao { get => datapublicacao; set => datapublicacao = value; }
+        public Autor Autor { get => autor; set => autor = value; }
+    }
+}
